edc635504e3de1e3befa019c590429728cb4347c
SANDIP KUUMAR DAS <115800844+sandipdas2278@users.noreply.github.com>
2024-08-08 02:19:45 +0530

    Create Variables.tf

---
+++ Variables.tf
@@ -0,0 +1,9 @@
+variable "frontend_script" {
+  description = "Path to the frontend script"
+  type        = string
+}
+
+variable "backend_script" {
+  description = "Path to the backend script"
+  type        = string
+}
