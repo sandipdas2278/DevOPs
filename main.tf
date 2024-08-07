4d121a347df73ebf42645530365c90e8275a0ff4
SANDIP KUUMAR DAS <115800844+sandipdas2278@users.noreply.github.com>
2024-08-08 02:12:43 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,72 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+}
+
+resource "aws_security_group" "public" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "frontend" {
+  ami           = "ami-0ad21ae1d0696ad58"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  security_groups = [aws_security_group.public.name]
+
+  provisioner "file" {
+    source      = "${var.frontend_script}"
+    destination = "/tmp/frontend.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/frontend.sh",
+      "/tmp/frontend.sh"
+    ]
+     }
+}
+
+resource "aws_instance" "backend" {
+  ami           = "ami-0ad21ae1d0696ad58"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private.id
+
+  provisioner "file" {
+    source      = "${var.backend_script}"
+    destination = "/tmp/backend.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/backend.sh",
+      "/tmp/backend.sh"
+    ]
+  }
+}
+
+output "frontend_ip" {
+  value = aws_instance.frontend.public_ip
+  }
